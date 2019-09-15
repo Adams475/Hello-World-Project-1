f5bf33c426b50d0fc1d57d600bf9f03afc813c3a
Noah Adams <adams475@purdue.edu>
2019-09-15 04:57:11 -0400

    Added Humidity Input

--- fan-website/src/FanProject.js
+++ fan-website/src/FanProject.js
@@ -6,7 +6,9 @@ import './App.css';
 
 class FanProject extends Component {
   state={
-    isSignedIn: false
+    isSignedIn: false,
+    value: "",
+    humidity: 0
   }
 
   uiConfig = {
@@ -25,20 +27,52 @@ class FanProject extends Component {
     })
   }
 
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({value: event.target.value});
+    console.log(this.state.humidity);
+  }
+
+  DisplayNumber = (event) =>{
+    event.preventDefault();
+    this.setState({value: event.target.value});
+    this.state.humidity = this.state.value;
+  }
+
   render(){
     return(
       <div className="app">
         {this.state.isSignedIn ?
+          //This is the logged in screen
           <div>
             <div className = "SignIn"><h1>Signed In!</h1>
-              <div><button type="button" class="btn btn-primary" onClick={()=>firebase.auth().signOut()}>Sign Out</button></div>
-            <div>Welcome {firebase.auth().currentUser.displayName}</div>
+
+            <div className = "welcome"><h4> Welcome {firebase.auth().currentUser.displayName}</h4></div>
+            <div className = "introText">
+              <p>
+                <span>This allows you to control your fan, and choose when you want it to turn on based on the humidity. </span>
+                <span>Please input the humidity level at which you want the fan to turn on.</span>
+              </p>
+              <div>
+                <form>
+                  <input type = "text" value={this.state.value} onChange={this.handleChange}>
+                  </input><button onClick={this.DisplayNumber}>Enter</button>
+                </form><dev className = "humidityNumber">{this.state.humidity}</dev>
+              </div>
+            </div>
+
+
+
+
+
+            <div className = "exitButton"><button type="button" className="btn btn-primary" onClick={()=>firebase.auth().signOut()}>Sign Out</button></div>
             </div>
           </div>
           :
-          <StyledFirebaseAuth className="googleSignIn" uiConfig={this.uiConfig}
+          //This is the log in screen
+          <div className = "title"><h1>Welcome to FanController</h1><StyledFirebaseAuth className="googleSignIn" uiConfig={this.uiConfig}
           firebaseAuth={firebase.auth()}
-          />
+          /></div>
         }
       </div>
     )
